3548185724e6e585cdb47f09cffea60e33455f55
alaraiabdiallah <alaraiabdiallah@gmail.com>
2023-03-19 21:39:01 +0700

    add home page

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+
+
+@extends('layouts.app')
+
+@section('title', 'Homepage')
+
+@section('content')
+    <h1>Homepage</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/home', function () {
+    return view('home');
+});
+
 Route::get('/register', [AuthController::class, "register"])->name('register');
 Route::get('/login', [AuthController::class, "login"])->name('login');
 
